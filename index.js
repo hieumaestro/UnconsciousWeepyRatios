9b52677455cb2c9787b6b2acaf49d62b1ea1b6d0
Hieu Tran Minh <54903623+hieumaestro@users.noreply.github.com>
2019-10-06 17:18:26 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var a = '1000';
+var b = '200';
+console.log(a > b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù 1000 trông có vẻ lớn hơn 200. Viết câu trả lời ở dưới đây.
+// Do khi so sánh chuỗi, phép toán sẽ so sánh từng ký tự trong chuỗi với nhau. Khi so sánh 2 chuỗi, kí tự đầu a ='1' < '2' = kí tự đầu của b nên kết quả là false
